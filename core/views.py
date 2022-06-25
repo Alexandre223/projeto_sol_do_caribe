0d1c6d54b6a5e8dbb02d97ffd6ff88f2fd3e521b
alexandre <alexandrechagas45@gmail.com>
2022-06-25 19:04:36 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+return render(request, 'index.html')
+
+def termos_e_servicos(request):
+return render(request, 'termos_e_servicos.html')
+
+def politica_de_privacidade(request):
+return render(request, 'politica_de_privacidade.html')
+
+def contato(request):
+return render(request, 'contato.html')
+
+def agendamento(request):
+return render(request, 'agendamento.html')
+# Create your views here.
